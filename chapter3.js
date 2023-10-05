eb6a25d7de7a0606ee7d0a30cdcf4b3b1b748052
shivanshCaba <webexpert@caba.in>
2023-10-05 17:13:01 +0530

    for,for-in,for-of(string,array,object basics)

--- chapter3.js
+++ chapter3.js
@@ -47,7 +47,8 @@ while(x<=10){
 }
 
 
-// iterables -> string, arrays
+// iterables -> string, arrays (for, for-of)
+// non-iterables->objects (for-in)
 // string -> 'a-z', 'A-z' or 'a-z + numbers' or 'a-z + numbers+ special characters'
 
 //          0123456789 
@@ -59,4 +60,76 @@ for(let i=0;i<19;i++){
     console.log(fact[i])
 }
 
-// objects
+// objects -> works in key-value pairs
+let car = {
+    name : "lamborghini",
+    color: "white",
+    speed: 200
+}
+
+// console.log(typeof(car));
+// console.log(car.name);
+// console.log(car.color);
+// console.log(car["speed"]);
+
+
+// Arrays -> type object
+
+let arr = [ 23, 45, 67, "shivy" , 67868,  {name: "shivansh", height: "5'9"}  ]
+
+// console.log(arr)
+
+// console.log(arr[0])
+// console.log(arr[3])
+// console.log(arr[5].name);
+
+// let hoshiyar = arr[5]
+// console.log(hoshiyar.name)
+
+
+// Arrays and loops
+let arr1 = [ 23, 45, 67, "shivy" , 67868,  {name: "shivansh", height: "5'9"}  ]
+
+// console.log(arr1[0])
+// console.log(arr1[1])
+// console.log(arr1[2])
+// console.log(arr1[3])
+// console.log(arr1[4])
+// console.log(arr1[5])
+
+// for(let i=0; i<6; i++){
+//     console.log(arr1[i])
+// }
+//  for - of
+// for (let item of arr1){
+//     console.log(item)
+// }
+
+// objects and loops
+
+
+
+let car1 = {
+    name : "lamborghini",
+    color: "white",
+    speed: 200
+}
+// console.log(car1.name);
+// console.log(car1.color);
+// console.log(car1["speed"]);
+
+// wrong loop
+// for(let i=0;i<3;i++){
+//     console.log(car1.i); // -> i = 0, 1, 2 -> car1.0 car1.1 car1.2
+// }
+
+// wrong loop
+// for(let item of car1){
+//     console.log(item);
+// }
+
+// for - in
+
+for(let i in car1){
+    console.log(car1[i])
+}
