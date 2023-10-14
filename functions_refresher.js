9be0c1f674591ef1df914dd097013c6f9e835824
shivanshCaba <webexpert@caba.in>
2023-10-14 16:55:49 +0530

    function revision

---
+++ functions_refresher.js
@@ -0,0 +1,99 @@
+// Functions-> To make dynamic code
+
+// WAP to print table of 2
+
+// for(let i=1;i<=10;i++){
+//     console.log(2*i);
+// }
+// for(let i=1;i<=10;i++){
+//     console.log(3*i);
+// }
+// for(let i=1;i<=10;i++){
+//     console.log(4*i);
+// }
+
+// Function -> Function Definition and Function Calling(Invoking)
+
+// function Declaratio
+// function func_name (parameters1, parameter2 ........ n){
+//     function body
+// }
+
+// function addition(kal, parso){
+//     console.log(kal + parso);
+// }
+
+
+// // function calling
+// addition(23, 45);
+
+
+// WAP to print table of differnet numbers
+
+// function table(num){
+//     for(let i=1;i<=10;i++){
+//         console.log(num*i)
+//     }
+// }
+
+
+// table(3);
+
+
+// let user=[2,3,4,5,6];
+
+// for(let i=0;i<user.length;i++){
+//     // table(user[i])
+//     let ele = user[i];
+//     table(ele);
+// }
+
+// return statement in functions
+
+// function goldDigger(paisa){
+//     console.log("Bought clothes of rs", paisa)
+// }
+
+// goldDigger(1000);
+
+// function addition(a,b){
+//     console.log(a+b)
+//     let ans = a+b;
+//     return ans;
+// }
+
+// console.log(addition(66,33));
+// let friend = addition(22,44);
+// console.log(friend);
+
+// WAP to print array of square of given numbers
+
+// let user = [2,3,4,5,6];
+
+// let ans = [];
+
+// for(let i=0;i<user.length;i++){
+//     // let item = user[i];
+//     //  ans.push(item * item)
+//     ans.push(user[i] * user[i])
+// }
+
+// console.log(ans)
+let user = [2,3,4,5,6];
+let ans = [];
+// function square(item, ans){
+//     ans.push(item * item)
+// }
+
+// for(let i=0;i<user.length;i++){
+//     square(user[i], ans)
+// }
+// console.log(ans);
+// function square(item, valueArr){
+//     valueArr.push(item * item)
+// }
+
+// for(let i=0;i<user.length;i++){
+//     square(user[i], ans)
+// }
+// console.log(ans);
