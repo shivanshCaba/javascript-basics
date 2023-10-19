b3483c32e12e7084193603e7165ca3ab1319fd7d
shivanshCaba <webexpert@caba.in>
2023-10-19 17:14:42 +0530

    arrays_methods and objects

--- arrays_methods.js
+++ arrays_methods.js
@@ -3,10 +3,28 @@
 // map()
 
 let arr = [55,33,22,11];
-arr.map(function(item){console.log(item*item)})
+// arr.map(function(item){console.log(item*item)})
+// arr=arr.map(function(item){return (item*item)})
+console.log(arr)
 
 // filter()-> this only returns the elements from an arrya which obey the true condition
-arr = arr.filter(function(item){
-    return item%2==0;
-})
-console.log(arr)
+// arr = arr.filter(function(item){
+//     return item%2==0;
+// })
+// console.log(arr);
+
+// .reduce()-> reduces the array into a single value
+
+// arr = arr.reduce(function(ans,item){
+//     return ans+item;
+// })
+
+// let ans = 1;
+// for(let i=0;i<arr.length;i++){
+//     ans = ans*arr[i];
+// }
+// console.log(ans)
+// console.log(arr)
+
+// .forEach()
+// arr.forEach(function(item){console.log(item+item)})
